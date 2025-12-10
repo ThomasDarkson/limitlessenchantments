8ac8cea342f1077d71fbd9a1bff2965fd6e12471
Thomas Darkson <152335332+ThomasDarkson@users.noreply.github.com>
2025-12-10 06:16:24 +0300

    Bump version to 2.0.0

--- common/src/main/java/limitless/enchantments/EnchantmentInterface.java
+++
@@ -1,9 +0,0 @@
-package limitless.enchantments;
-
-import java.util.OptionalInt;
-
-public interface EnchantmentInterface {
-    OptionalInt getOptionalInt();
-    int getOgMaxLevel();
-    void setOgMaxLevel(int level);
-}
